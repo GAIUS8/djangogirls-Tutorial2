368f85851d040ae4807bc9d3b7899bc7e0cfb275
GAIUS8 <gaius827@gmail.com>
2017-05-30 17:43:24 +0900

    post-modyfy added

---
+++ django_app/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+class PostCreateForm(forms.Form):
+    title = forms.CharField(
+        label='제목',
+        max_length=10,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-group'
+            }
+        )
+
+    )
+    text = forms.CharField(label='내용', widget=forms.Textarea, required=True)
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
 
+from .forms import PostCreateForm
+
 User = get_user_model()
 
 
@@ -31,18 +33,45 @@ def post_detail(request, pk):
 
 def post_create(request):
     if request.method == 'GET':
+        form = PostCreateForm()
         context = {
-
+            'form': form,
         }
         return render(request, 'blog/post_create.html', context=context)
+
     elif request.method == 'POST':
+        # Form클래스의 생성자에 POST데이터를 전달하여 Form인스턴스를 생성
+        form = PostCreateForm(request.POST)
+        # Form인스턴스의 유효성을 검사하는 is_valid메서드
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            text = form.cleaned_data['text']
+            user = User.objects.first()
+            post = Post.objects.create(
+                title=title,
+                text=text,
+                author=user,
+            )
+            return redirect('post_detail', pk=post.pk)
+        else:
+            context = {
+                'form': form,
+            }
+            return render(request, 'blog/post_create.html', context=context)
+
+
+def post_modify(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':  # 수정하기 눌러서 요청이 들어왔을 때
         data = request.POST
         title = data['title']
         text = data['text']
-        user = User.objects.first()
-        post = Post.objects.create(
-            title=title,
-            text=text,
-            author=user,
-        )
+        post.title = title
+        post.text = text
+        post.save()
         return redirect('post_detail', pk=post.pk)
+    elif request.method == 'GET':  # 링크나 브라우저를 통해서 왔을 때
+        context = {
+            'post': post,
+        }
+        return render(request, 'blog/post_modify.html', context)
