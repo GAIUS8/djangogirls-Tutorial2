2460efd47c9e3c8f07845a6e84b2129b3f8e9058
GAIUS8 <gaius827@gmail.com>
2017-05-30 12:40:16 +0900

    create base template base.html, post_list make lint to post_detail

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
+
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()
@@ -23,4 +24,4 @@ def post_detail(request, pk):
     context = {
         'post': posts,
     }
-    return render(request,'blog/post_detail.html', context=context)
+    return render(request, 'blog/post_detail.html', context=context)
